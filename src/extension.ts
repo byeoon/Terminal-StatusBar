72b7a0b3dddd476b9431037ab901bee96cbacd6f
byeoon <47872200+byeoon@users.noreply.github.com>
2025-02-03 17:47:13 +0000

    feat: fixed plugin now works, removed about thing tho

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,13 @@ let statusBar: vscode.StatusBarItem;
 var terminalArray = vscode.workspace.getConfiguration('terminal-statusbar').get('terminalArray');
 
 export function activate(context: vscode.ExtensionContext) {
-	const openTerminal = 'terminal-statusbar.openTerminal';
 	console.log('Activated Terminal StatusBar!');
 
-	statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	statusBar.command = openTerminal;
+	statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 50);
+	statusBar.command = "terminal-statusbar.openTerminal";
 	statusBar.text = `$(terminal-view-icon) Terminal (${vscode.window.terminals[0].name})`;
 	statusBar.tooltip = `Opens the terminal.`;
 	statusBar.show();
-
-	const about = vscode.commands.registerCommand('terminal-statusbar.about', () => {
-		vscode.window.showInformationMessage('v1.0.3 - Fixed bug where it wouldnt load on start, added image.');
-		vscode.window.showInformationMessage("Terminal Array Setting: " + terminalArray);
-	});
 
 	const openStatusBar = vscode.commands.registerCommand('terminal-statusbar.openTerminal', () => {
 		vscode.window.terminals[0].show();
@@ -24,9 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	
 	context.subscriptions.push(openStatusBar);
-	context.subscriptions.push(about);
 }
-
 export function deactivate() {
 	console.log("Terminal StatusBar is now deactivated.");
 }
